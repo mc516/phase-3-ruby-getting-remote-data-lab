0cef2313172efdbad166961445a6b400e2639e5f
Mickey <mickeyc11111@gmail.com>
2022-10-01 20:11:19 -0400

    commit tree 9f1b18a710d87a3fbe30af9e19238672db1ee1b1

--- lib/get_requester.rb
+++ lib/get_requester.rb
@@ -1,1 +1,26 @@
 # Write your code here
+require 'net/http'
+require 'open-uri'
+require 'json'
+
+class GetRequester
+    # URL = "http://data.cityofnewyork.us/resource/uvks-tn5n.json"
+
+    def initialize(url)
+        @url = url
+    end 
+
+    def get_response_body
+        uri  = URI.parse(@url)
+        response = Net::HTTP.get_response(uri)     
+        response.body
+    end
+
+    def parse_json
+        parsed = JSON.parse(self.get_response_body)
+    end
+
+
+
+
+end
